e44a4610f9093192f50eeb3fbe2cd5a9dade4f60
Arnout Boelens <ampboelens@gmail.com>
2021-09-27 14:44:10 -0700

    Fixed version in documentation

--- docs-config/conf.py
+++ docs-config/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Arnout Boelens'
 author = 'Arnout Boelens'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.2.4'
 
 
 # -- General configuration ---------------------------------------------------

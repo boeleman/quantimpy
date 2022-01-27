2487ad556c9c032531259a7544e1eb5775a6630f
Arnout Boelens <ampboelens@gmail.com>
2022-01-26 19:44:26 -0800

    Bumped version to 0.3.1

--- docs-config/conf.py
+++ docs-config/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Arnout Boelens'
 author = 'Arnout Boelens'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.1'
 
 
 # -- General configuration ---------------------------------------------------

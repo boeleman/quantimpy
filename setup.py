b57dc5e5c03cca2256543655a33ac4a0bdffb2f1
Arnout Boelens <ampboelens@gmail.com>
2022-04-19 10:48:05 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extensions = [
 
 setup(
     name="quantimpy",
-    version="0.4.0",
+    version="0.4.1",
     description="This package performs morphological operations and can compute the Minkowski functionals and functions",
     long_description=README,
     long_description_content_type="text/markdown",

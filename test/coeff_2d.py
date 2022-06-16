879347536551789820474c93de479a67c6c74033
Arnout Boelens <ampboelens@gmail.com>
2022-06-16 15:55:30 -0700

    Finished initial help functions for BRISQUE

---
+++ test/coeff_2d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import numpy as np
+from quantimpy import brisque as bq
+
+# Load data
+image = np.load("rock_2d.npy")
+
+# Compute MSCN coefficients
+mscn = bq.mscn(image)
+
+print(bq.coeff(mscn))
